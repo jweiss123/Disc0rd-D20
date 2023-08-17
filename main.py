9c162da63add50ff2a8defedfb01a13b023d9019
jweiss123 <25529297-jweiss123@users.noreply.replit.com>
2023-08-17 18:45:49 +0000

    New dice
    Now includes D4, D8, D10, D12, and D100 dice rolls

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from keep_alive import keep_alive
 
 keep_alive()
 
+version = discord.__version__
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
@@ -16,7 +17,9 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
-  print(f'Logged in as {bot.user.name} ({bot.user.id})')
+  print(
+    f'Logged in as {bot.user.name} ({bot.user.id}), Discord version: {version}'
+  )
 
 
 @bot.event
@@ -31,7 +34,7 @@ async def on_message(message):
   await bot.process_commands(message)
 
 
-@bot.slash_command(name='d20', description='Rolls a twenty-sided die')
+@bot.command(name='d20', description='Rolls a twenty-sided die')
 async def d20(ctx):
   rolled_num = random.randint(1, 20)
   if rolled_num == 1:
@@ -40,9 +43,54 @@ async def d20(ctx):
     await ctx.send(f':game_die: You rolled a {rolled_num}!')
 
 
-@bot.slash_command(name='d6', description='Rolls a six-sided die')
+@bot.command(name='d12', description='Rolls a twelve-sided die')
+async def d12(ctx):
+  rolled_num = random.randint(1, 12)
+  if rolled_num == 1:
+    await ctx.send(f':game_die: You rolled a {rolled_num}... :cry:')
+  else:
+    await ctx.send(f':game_die: You rolled a {rolled_num}!')
+
+
+@bot.command(name='d10', description='Rolls a ten-sided die')
+async def d10(ctx):
+  rolled_num = random.randint(1, 10)
+  if rolled_num == 1:
+    await ctx.send(f':game_die: You rolled a {rolled_num}... :cry:')
+  else:
+    await ctx.send(f':game_die: You rolled a {rolled_num}!')
+
+
+@bot.command(name='d8', description='Rolls an eight-sided die')
+async def d8(ctx):
+  rolled_num = random.randint(1, 8)
+  if rolled_num == 1:
+    await ctx.send(f':game_die: You rolled a {rolled_num}... :cry:')
+  else:
+    await ctx.send(f':game_die: You rolled a {rolled_num}!')
+
+
+@bot.command(name='d6', description='Rolls a six-sided die')
 async def d6(ctx):
   rolled_num = random.randint(1, 6)
+  if rolled_num == 1:
+    await ctx.send(f':game_die: You rolled a {rolled_num}... :cry:')
+  else:
+    await ctx.send(f':game_die: You rolled a {rolled_num}!')
+
+
+@bot.command(name='d4', description='Rolls a four-sided die')
+async def d4(ctx):
+  rolled_num = random.randint(1, 4)
+  if rolled_num == 1:
+    await ctx.send(f':game_die: You rolled a {rolled_num}... :cry:')
+  else:
+    await ctx.send(f':game_die: You rolled a {rolled_num}!')
+
+
+@bot.command(name='d100', description='Rolls a hundred-sided die')
+async def d100(ctx):
+  rolled_num = random.randint(1, 100)
   if rolled_num == 1:
     await ctx.send(f':game_die: You rolled a {rolled_num}... :cry:')
   else:
